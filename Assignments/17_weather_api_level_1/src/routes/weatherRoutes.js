785422121f01cff46fffc872c7a4084c6af1a8ab
vinayaka0305 <vav6473@gmail.com>
2024-12-16 22:54:17 +0530

    code

--- Assignments/17_weather_api_level_1/src/routes/weatherRoutes.js
+++ Assignments/17_weather_api_level_1/src/routes/weatherRoutes.js
@@ -5,9 +5,7 @@ const weatherController = require("../controllers/weatherController");
 router.get("/city/:name", async (req, res) => {
   try {
     const cityName = req.params.name;
-    // const weatherData = await weatherController.getWeatherDataByName(cityName);
-    // const weatherData = await weatherController.getForecaseDataByName(cityName)
-    const weatherData = await weatherController.getZipCodeByCityName(cityName)
+    const weatherData = await weatherController.getWeatherDataByName(cityName);
     res.status(200).json({
       statuse: "success",
       message: "Weather data retrieved",
@@ -22,5 +20,42 @@ router.get("/city/:name", async (req, res) => {
     });
   }
 });
+router.get("/forecast/city/:name", async (req, res) => {
+    try {
+      const cityName = req.params.name;
+      const weatherData = await weatherController.getForecaseDataByName(cityName)
+      res.status(200).json({
+        statuse: "success",
+        message: "Weather data retrieved",
+        data: weatherData,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        status: "error",
+        message: "Failed to retrieve weather data",
+        error: error.message,
+      });
+    }
+  });
+
+  router.get("/city/zipcode/:code", async (req, res) => {
+    try {
+      const cityName = req.params.code;
+      const weatherData = await weatherController.getZipCodeByCityName(cityName)
+      res.status(200).json({
+        statuse: "success",
+        message: "Weather data retrieved",
+        data: weatherData,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        status: "error",
+        message: "Failed to retrieve weather data",
+        error: error.message,
+      });
+    }
+  });
 
 module.exports = router;
