2a358196a164b57ecbf289713c854b3e27f8cd69
vinayaka0305 <vav6473@gmail.com>
2024-12-21 20:47:50 +0530

    code

--- 14_mvc_pattern/mvc/src/app.js
+++ 14_mvc_pattern/mvc/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const tourRoutes = require('./routes/tourRoutes');
- 
-app.use(express.json());
+// const path = require('path');
 
+// app.use('',express.static(path.join(__dirname,'views')))
+app.use('',express.static(`${__dirname}/views`))
+
+app.use(express.json());
 app.use('/api/v1/tours',tourRoutes);
+
 
 
 module.exports = app;
